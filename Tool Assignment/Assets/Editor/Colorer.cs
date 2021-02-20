8d7fed538971dd2a86033c4962fdfe232109e868
Atreas <andrikos2001@gmail.com>
2021-02-20 15:31:19 +0200

    Uploaded the tool

---
+++ Tool Assignment/Assets/Editor/Colorer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public class Colorer : EditorWindow
+ {
+    
+    Color color;
+    [MenuItem("Window/Color them all")]
+    public static void ShowWindow()
+    {
+      GetWindow<Colorer>("Colorer");
+    }
+
+    private void OnGUI()
+    {
+
+       GUILayout.Label("Color them all", EditorStyles.boldLabel);
+
+            
+        color = EditorGUILayout.ColorField("Color", color);
+      
+        if (GUILayout.Button("PAINT THEM ALL"))
+        {
+            ColorThem();
+
+        }
+
+    }
+
+    private void ColorThem()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.sharedMaterial.color = color;
+            }
+        }
+    }
+}
